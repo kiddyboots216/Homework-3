fe8f41b4ec9709a5fe351a2e81c2f93c469e42ca
Ashwinee Panda <kiddyboots216@berkeley.edu>
2017-10-14 22:57:55 -0700

    Updated BadAuction.sol
    writing code in github directly because my text editor is hung doing 189 LMAO

--- contracts/BadAuction.sol
+++ contracts/BadAuction.sol
@@ -11,11 +11,24 @@ contract BadAuction is AuctionInterface {
 	 * their funds back
 	 */
 	function bid() payable external returns (bool) {
-		// YOUR CODE HERE
+		if(msg.value<=highestBid) {
+			if(!msg.sender.send(msg.value)) {
+				throw;
+				}
+			return false;
+		}
+		if(!highestBidder.send(highestBid)) {
+			if(!msg.sender.send(msg.value)) {
+				throw;
+				}
+			return false;
+		}
+		highestBidder = msg.sender;
+		highestBid = msg.value;
 	}
 
 	/* Give people their funds back */
 	function () payable {
-		// YOUR CODE HERE
+		msg.sender.transfer(msg.value);
 	}
 }
